6669594aa9e5dbe0d6e2feafea62b8004a742749
Mario Gualtieri <mario.gualtieri2012@gmail.com>
2018-02-14 23:00:09 +0100

    Add ReactiveHttpClient

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,20 +1,21 @@
-import { Injectable } from '@angular/core';
-import { SocketIoModule, Socket } from 'ng-socket-io';
+import {Injectable} from '@angular/core';
+import {SocketIoModule, Socket} from 'ng-socket-io';
 
 
 @Injectable()
 export class SocketService {
 
-  constructor(private socket: Socket) { }
+  constructor(private socket: Socket) {
+  }
 
-  public sendMessage(messageName: string, msg: any){
+  public sendMessage(messageName: string, msg: any) {
     this.socket.emit(messageName, msg);
   }
 
-public getMessage(event: string) {
+  public getMessage(event: string) {
     return this.socket
-        .fromEvent(event)
-        // .map( data => data.msg )
-        ;
+      .fromEvent(event)
+      // .map( data => data.msg )
+      ;
   }
 }
