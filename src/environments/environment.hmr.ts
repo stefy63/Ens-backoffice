61ff0d197587a30e265ac3285ee8a44f12747e3b
Stefano <stefano.scarsella@3punto6.com>
2018-02-28 15:19:38 +0100

    set production enviroment

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -1,4 +1,15 @@
 export const environment = {
     production: false,
-    hmr       : true
+    hmr       : true,
+
+    
+    // Socket.io configuration url
+    ws_url: 'http://localhost',
+    ws_suffix: '',
+    ws_port: 9000,
+
+    // API configuration url
+    api_url: 'http://localhost',
+    api_port: 3000,
+    api_suffix: '/api',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr       : false,
+
+    
+    // Socket.io configuration url
+    ws_url: 'http://localhost',
+    ws_suffix: '',
+    ws_port: 9000,
+
+    // API configuration url
+    api_url: 'http://localhost',
+    api_port: 3000,
+    api_suffix: '/api',
+    
 };
