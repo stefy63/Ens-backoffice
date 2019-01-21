45f45a415df555d61292400a837b1cf02b2f6a7f
Stefano <stefano.scarsella@3punto6.com>
2019-01-21 18:01:22 +0100

    Add ù in alphabetic only validator

--- src/app/main/services/MaterialValidator/AlphabeticOnlyValidator.ts
+++ src/app/main/services/MaterialValidator/AlphabeticOnlyValidator.ts
@@ -2,7 +2,7 @@ import {AbstractControl} from '@angular/forms';
 
 export class AlphabeticOnlyValidator {
   static alphabeticOnly(AC: AbstractControl) {
-    if (AC && AC.value && !/^(?=.*[a-zA-Zàèéìò])[a-zA-Zàèéìò]+$/.test(AC.value )) {
+    if (AC && AC.value && !/^(?=.*[a-zA-Zàèéìòù])[a-zA-Zàèéìòù]+$/.test(AC.value )) {
         return {alphabeticOnly: true};
     }
     return null;
