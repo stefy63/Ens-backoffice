51873e3eeb862d29cb03f8574d342bf0d726e196
Mario Gualtieri <mario.gualtieri2012@gmail.com>
2019-01-21 23:38:42 +0100

    Fix DateValidator when date is null

--- src/app/main/services/MaterialValidator/DateValidator.ts
+++ src/app/main/services/MaterialValidator/DateValidator.ts
@@ -3,7 +3,7 @@ import * as moment from 'moment';
 
 export class DateValidator {
   static date(AC: AbstractControl) {
-    if (!!AC.value._i) {
+    if (AC.value && AC.value._i) {
       const testInput = (!!AC.value._i) ? AC.value._i : '00/00/0000';
       const testRegx = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(testInput);
       console.log(AC.value, testInput);
