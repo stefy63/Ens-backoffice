40d11c407035bb4b5d202b5b7965c56578df8fa1
Stefano <stefano.scarsella@3punto6.com>
2019-04-29 17:29:42 +0200

    Bug Fix

--- src/app/main/services/MaterialValidator/AlphabeticOnlyValidator.ts
+++ src/app/main/services/MaterialValidator/AlphabeticOnlyValidator.ts
@@ -2,7 +2,7 @@ import {AbstractControl} from '@angular/forms';
 
 export class AlphabeticOnlyValidator {
   static alphabeticOnly(AC: AbstractControl) {
-    if (AC && AC.value && !/^(?=.*[A-zÀ-ú])[A-zÀ-ú]+$/.test(AC.value )) {
+    if (AC && AC.value && !/^[A-zÀ-ú]+([\sA-zÀ-ú]+)?$/.test(AC.value )) {
         return {alphabeticOnly: true};
     }
     return null;
