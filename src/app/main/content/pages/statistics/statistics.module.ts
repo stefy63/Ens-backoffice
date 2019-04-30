7880eb41e6ec0a2a9ab3f7c11b5a4028e19eb1b2
Stefano <stefano.scarsella@3punto6.com>
2019-04-30 13:07:20 +0200

    Add skeleton for statistics

---
+++ src/app/main/content/pages/statistics/statistics.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatisticsComponent } from './statistics.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [StatisticsComponent]
+})
+export class StatisticsModule { }
